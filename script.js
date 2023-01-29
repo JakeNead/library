85575bbedc5d63138ea8c5e266164d1470692ce8
Jake Nead <jakenead@gmail.com>
2023-01-29 11:31:20 -0800

    Add constructor function

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const myLibrary = [];
 
-function book() {
-
+function Book(title, author, pageNumber, hasRead) {
+  const book = Object.create(Book.prototype)
+  this.title = title
+  this.author = author
+  this.pageNumber = pageNumber
+  this.hasRead = hasRead
+  return book
 }
 
 function addBookToLibrary() {
