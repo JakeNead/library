0cf2ae2dae91387e37dda033906c0119667dcfbb
Jake Nead <jakenead@gmail.com>
2023-01-29 20:27:58 -0800

    Add placeholder book objects to the MyLibrary array

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const myLibrary = [];
+const myLibrary = [{
+  title: 'The Final Empire',
+  author: 'Brandon Sanderson',
+  pageNumber: 541,
+  hasRead: true,
+},
+{
+  title: 'The Tao of Pooh',
+  author: 'Benjamin Hoff',
+  pageNumber: 158,
+  hasRead: true,
+},
+{
+  title: 'East of Eden',
+  author: 'John Steinbeck',
+  pageNumber: 608,
+  hasRead: true,
+}];
 
 function Book(title, author, pageNumber, hasRead) {
   const book = Object.create(Book.prototype)
