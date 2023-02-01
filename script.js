372d41c54725a8f8c5a3f989f11b1b316eaf5b65
Jake Nead <jakenead@gmail.com>
2023-01-31 18:12:12 -0800

    Add event listener to the form submit button to prevent page refresha and to store user input

--- script.js
+++ script.js
@@ -1,31 +1,60 @@
-const myLibrary = [{
+const form = document.querySelector("#addBook")
+const inputs = document.querySelectorAll('input')
+const submitButton = document.getElementById('submit')
+const title = document.getElementById('title')
+const author = document.getElementById('author')
+const pages = document.getElementById('pages')
+const hasRead = document.getElementById('read')
+
+const bookList = [{
   title: 'The Final Empire',
   author: 'Brandon Sanderson',
   pageNumber: 541,
   hasRead: true,
-},
-{
-  title: 'The Tao of Pooh',
-  author: 'Benjamin Hoff',
-  pageNumber: 158,
-  hasRead: true,
-},
-{
-  title: 'East of Eden',
-  author: 'John Steinbeck',
-  pageNumber: 608,
-  hasRead: true,
-}];
+}]
 
-function Book(title, author, pageNumber, hasRead) {
+// {
+//   title: 'The Tao of Pooh',
+//   author: 'Benjamin Hoff',
+//   pageNumber: 158,
+//   hasRead: true,
+// },
+// {
+//   title: 'East of Eden',
+//   author: 'John Steinbeck',
+//   pageNumber: 608,
+//   hasRead: true,
+// }
+
+function Book(bookTitle, bookAuthor, pageNumber, hasReadBook) {
   const book = Object.create(Book.prototype)
-  this.title = title
-  this.author = author
+  this.title = bookTitle
+  this.author = bookAuthor
   this.pageNumber = pageNumber
-  this.hasRead = hasRead
+  this.hasRead = hasReadBook
   return book
 }
 
 function addBookToLibrary() {
+  // take user input
+
+
+  // run constructor func
+  // push new obj to lib array
+  // return bookList.push(Book())
+}
+
+form.addEventListener('submit', (e) => {
+   e.preventDefault();
+  bookList.push(Book(title.value, author.value, pages.value, hasRead.checked))
+  console.log(title.value, author.value, pages.value, hasRead.checked)
 
 }
+)
+
+
+// function displayBooks() {
+
+// }
+
+
