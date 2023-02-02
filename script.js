279f173efc47f0f5430e2defc1fa721b10ecb418
Jake Nead <jakenead@gmail.com>
2023-02-01 19:08:24 -0800

    First draft adding user input to new elements

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("#addBook")
-const inputs = document.querySelectorAll('input')
-const submitButton = document.getElementById('submit')
+// const inputs = document.querySelectorAll('input')
+// const submitButton = document.getElementById('submit')
 const title = document.getElementById('title')
 const author = document.getElementById('author')
 const pages = document.getElementById('pages')
@@ -23,26 +23,53 @@ const bookList = [{
 //   title: 'East of Eden',
 //   author: 'John Steinbeck',
 //   pageNumber: 608,
-//   hasRead: true,
+//   hasRead: false,
 // }
 
 function Book(bookTitle, bookAuthor, pageNumber, hasReadBook) {
-  const book = Object.create(Book.prototype)
   this.title = bookTitle
   this.author = bookAuthor
   this.pageNumber = pageNumber
   this.hasRead = hasReadBook
-  return book
 }
+
+
 form.addEventListener('submit', (e) => {
    e.preventDefault();
-  bookList.push(Book(title.value, author.value, pages.value, hasRead.checked))
+  bookList.push(new Book(title.value, author.value, pages.value, hasRead.checked));
+  const articleContainer = document.getElementsByClassName('articleContainer')
+  const newArticle = document.createElement('Article')
+  articleContainer.appendChild(newArticle)
+  const newDiv = document.createElement('div')
+  newDiv.textContent = title.value
+  newArticle.appendChild(newDiv)
+  newDiv.textContent = author.value
+  newArticle.appendChild(newDiv)
+  newDiv.textContent = pages.value
+  newArticle.appendChild(newDiv)
+
 }
 )
 
 
 // function displayNewBook() {
-
+//   bookList.forEach(obj => )
 // }
 
+// const articleContainer = document.getElementsByClassName('articleContainer')
+// const newArticle = document.createElement('Article')
+// articleContainer.appendChild(newArticle)
+// const newDiv = document.createElement('div')
+// newDiv.textContent = title.value
+// newArticle.appendChild(newDiv)
+// newDiv.textContent = author.value
+// newArticle.appendChild(newDiv)
+// newDiv.textContent = pages.value
+// newArticle.appendChild(newDiv)
 
+  // search for articleContainer
+  // create new article element
+  // append new article to articleContainer
+  // create new divs
+// add content to divs
+// append divs to article element
