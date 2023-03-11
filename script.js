92a2009d91172b6edc71fdc8dd2e8b08511e7be4
Jake Nead <jakenead@gmail.com>
2023-03-11 12:14:13 -0800

    Wrap module in IIFE

--- script.js
+++ script.js
@@ -1,110 +1,147 @@
 /* eslint-disable no-use-before-define */
 
-const library = {
-  bookList: [
-    {title: 'Sample Book Title',
-    author: 'Author\'s Name',
-    pages: 1234,
-    hasRead: true
-    }, 
-    {title: 'Sample Book Title',
-    author: 'Author\'s Name',
-    pages: 1234,
-    hasRead: true
-    },
-    {title: 'Sample Book Title',
-    author: 'Author\'s Name',
-    pages: 1234,
-    hasRead: true},
-],
+// Current bugs
+//    New book article doesn't display pages
+//    adding multiple books creates exponential articles
+//    removing books erases entire bookList array
 
-  init() {
-    this.cacheDom()
-    this.render()
-  },
-  cacheDom() {
-    this.form = document.getElementById('addBook')
-    this.title = document.getElementById('title')
-    this.author = document.getElementById('author')
-    this.pages = document.getElementById('pages')
-    this.read = document.getElementById('read')
-    this.articleContainer = document.querySelector(".articleContainer")
-    this.popupButton = document.querySelector('[data-popup-target]')
-    this.popup = document.querySelector('.popup')
-    this.overlay = document.getElementById('overlay')
-  },  
-  render() {
-    this.clearPage(this.articleContainer)
-    this.addBookElements()
-    this.bindEvents()
-  },
-  bindEvents() {
-    this.form.addEventListener('submit', e => {
-      e.preventDefault()
-      this.bookList.push(new Book(title.value, author.value, pages.value, read.checked))
+(function() {
+  const library = {
+    bookList: [
+      {title: 'Sample Book Title',
+      author: 'Author\'s Name',
+      pages: 1234,
+      hasRead: true
+      }, 
+      {title: 'Sample Book Title',
+      author: 'Author\'s Name',
+      pages: 1234,
+      hasRead: true
+      },
+      {title: 'Sample Book Title',
+      author: 'Author\'s Name',
+      pages: 1234,
+      hasRead: true},
+    ],
+
+    init() {
+      this.cacheDom()
       this.render()
-    })
-    this.popupButton.addEventListener('click', () => {
-      this.openPopup(this.popup)
-    })
-    this.overlay.addEventListener('click', () => {
-      this.closePopup(this.popup)
-    })
-      this.readToggle()
-  },
-  clearPage(node) {
-    while(node.hasChildNodes()) {
-      node.removeChild(node.firstChild)
-    }
-  },
-  addBookElements() { 
-    let n = 0
-    this.bookList.forEach(book => {    
-      this.newArticle = document.createElement("article")
-      this.articleContainer.appendChild(this.newArticle)   
-      const divContent = [book.title, book.author, book.pages, book.hasRead ? 'Read': 'Has not read']
-      const articleClasses = ['title', 'author', 'pages', (book.hasRead ? 'readButton hasRead': 'readButton hasNotRead')]
-    
-      for (let i = 0; i < divContent.length; i++) {
-        const div = document.createElement('div')
-        div.setAttribute('class', articleClasses[i])
-        div.textContent = divContent[i]
-        this.newArticle.append(div)
-      }
-    this.newArticle.setAttribute('data-index', (n+=1) )
-    const button = document.createElement('button')
-    button.setAttribute('class', 'remove')
-    button.textContent = 'Remove'
-    this.newArticle.append(button)
-    })
-  },
-  openPopup(popup) {
-    if (popup == null) return 
-      popup.classList.add('active')
-      this.overlay.classList.add('active')
-  },
-  closePopup(popup) {
-      if (popup == null) return 
-        this.popup.classList.remove('active')
-        this.overlay.classList.remove('active')
     },
-  readToggle(){
-    const readButton = document.querySelectorAll('.readButton')
-    readButton.forEach(btn => btn.addEventListener('click', () => {
-      if (btn.textContent === 'Read') {
-        btn.classList.remove('hasRead')
-        btn.classList.add('hasNotRead')
-        btn.textContent = 'Not Read'
-        this.bookList[btn.parentElement.getAttribute('data-index')-1].hasRead = false
-      } else {
-        btn.classList.remove('hasNotRead')
-        btn.classList.add('hasRead')
-        btn.textContent = 'Read'
-        this.bookList[btn.parentElement.getAttribute('data-index')-1].hasRead = true
-      } 
-      }))
+
+    cacheDom() {
+      this.form = document.getElementById('addBook')
+      this.title = document.getElementById('title')
+      this.author = document.getElementById('author')
+      this.pages = document.getElementById('pages')
+      this.read = document.getElementById('read')
+      this.articleContainer = document.querySelector(".articleContainer")
+      this.popupButton = document.querySelector('[data-popup-target]')
+      this.popup = document.querySelector('.popup')
+      this.overlay = document.getElementById('overlay')
+    },  
+
+    render() {
+      this.clearPage(this.articleContainer)
+      this.addBookElements()
+      this.bindEvents()
+    },
+
+    bindEvents() {
+      this.form.addEventListener('submit', e => {
+        e.preventDefault()
+        this.bookList.push(new Book(title.value, author.value, pages.value, read.checked))
+        this.closePopup(popup)
+        this.render()
+      
+        this.form.title.value = ''
+        this.form.author.value = ''
+        this.form.pages.value = ''
+        this.form.read.checked = false
+        console.log(this.bookList)
+      })
+      this.popupButton.addEventListener('click', () => {
+        this.openPopup(this.popup)
+      })
+      this.overlay.addEventListener('click', () => {
+        this.closePopup(this.popup)
+      })
+      this.readToggle()
+      this.removeBook()
+
+    },
+
+    clearPage(node) {
+      while(node.hasChildNodes()) {
+        node.removeChild(node.firstChild)
+      }
+    },
+
+    addBookElements() { 
+      let n = 0
+      this.bookList.forEach(book => {    
+        this.newArticle = document.createElement("article")
+        this.articleContainer.appendChild(this.newArticle)   
+        const divContent = [book.title, book.author, book.pages, book.hasRead ? 'Read': 'Has not read']
+        const articleClasses = ['title', 'author', 'pages', (book.hasRead ? 'readButton hasRead': 'readButton hasNotRead')]
+      
+        for (let i = 0; i < divContent.length; i++) {
+          const div = document.createElement('div')
+          div.setAttribute('class', articleClasses[i])
+          div.textContent = divContent[i]
+          this.newArticle.append(div)
+        }
+      this.newArticle.setAttribute('data-index', (n+=1) )
+      const button = document.createElement('button')
+      button.setAttribute('class', 'remove')
+      button.textContent = 'Remove'
+      this.newArticle.append(button)
+      })
+    },
+
+    openPopup(popup) {
+      if (popup == null) return 
+        popup.classList.add('active')
+        this.overlay.classList.add('active')
+    },
+
+    closePopup(popup) {
+        if (popup == null) return 
+          this.popup.classList.remove('active')
+          this.overlay.classList.remove('active')
+    },
+
+    readToggle(){
+      const readButton = document.querySelectorAll('.readButton')
+      readButton.forEach(btn => btn.addEventListener('click', () => {
+        if (btn.textContent === 'Read') {
+          btn.classList.remove('hasRead')
+          btn.classList.add('hasNotRead')
+          btn.textContent = 'Not Read'
+          this.bookList[btn.parentElement.getAttribute('data-index')-1].hasRead = false
+        } else {
+          btn.classList.remove('hasNotRead')
+          btn.classList.add('hasRead')
+          btn.textContent = 'Read'
+          this.bookList[btn.parentElement.getAttribute('data-index')-1].hasRead = true
+        } 
+        }))
+    },
+
+    removeBook () {
+      const remove = document.querySelectorAll('.remove')
+      remove.forEach(btn => btn.addEventListener('click', () => {
+        
+        if (window.confirm('Are you sure you want to permanently remove this book?')) {
+          this.bookList.splice(btn.parentElement.getAttribute('data-index')-1)
+          btn.parentElement.remove()
+        }
+        console.log(library.bookList)
+      })
+    )}
   }
-}
+  library.init()
+})()
 
 class Book {
   constructor (bookTitle, bookAuthor, pageNumber, hasReadBook) {
@@ -115,7 +152,6 @@ class Book {
   }
 }
 
-library.init()
 
 // const removeBookClick = () => {
 //   const remove = document.querySelectorAll('.remove')
